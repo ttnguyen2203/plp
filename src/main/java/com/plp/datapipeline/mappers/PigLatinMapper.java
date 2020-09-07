33fe918d824869d459fb6425485021bc86f52662
ttnguyen2203 <ttnguyen2203@berkeley.edu>
2020-09-06 17:11:18 -0700

    implement mapper

--- src/main/java/com/plp/datapipeline/mappers/PigLatinMapper.java
+++ src/main/java/com/plp/datapipeline/mappers/PigLatinMapper.java
@@ -1,16 +1,26 @@
 package com.plp.datapipeline.mappers;
 
 import com.plp.datapipeline.mappers.config.PigLatinMapperConfig;
+import org.springframework.lang.NonNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class PigLatinMapper extends IMapper<PigLatinMapperConfig> {
 
-    private static final Set<Character> VOWELS = new HashSet<>(
-            Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+    private static final Set<Character> FIRST_CHAR_VOWELS = new HashSet<>(
+            Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'y', 'Y'));
+    private static final String VOWEL_POST_FIX = "way";
+    private static final String CONSONANT_POST_FIX = "ay";
+
+    //todo: punctuation
 
     @Override
     public void init(final PigLatinMapperConfig config) {
@@ -19,8 +29,77 @@ public class PigLatinMapper extends IMapper<PigLatinMapperConfig> {
 
     @Override
     public Map<String, Object> map(final Map<String, Object> data) {
-        //todo: impl
+        return data.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Entry::getKey,
+                        e -> transformSentence((String) e.getValue())
 
-        return data;
+                ));
+    }
+
+    protected String transformSentence(@NonNull final String sentence) {
+        final List<String> transformed = Arrays.stream(sentence.split(" "))
+                .map(this::transformWord)
+                .collect(Collectors.toList());
+        return String.join(" ", transformed);
+
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    protected String transformWord(@NonNull final String word) {
+        if (word.length() < 1) {
+            return word;
+        }
+
+        //if not a word, don't convert
+        final char[] charArr = word.toCharArray();
+        for (char c : charArr) {
+            if (!isAlphabetic(c)) {
+                return word;
+            }
+        }
+
+        //handle case
+        final boolean upperCase = Character.isUpperCase(charArr[0]);
+        charArr[0] = Character.toLowerCase(charArr[0]);
+
+        if (FIRST_CHAR_VOWELS.contains(charArr[0])) {
+            // if word starts with a vowel, append "-yay" to the end of the word and return
+            return word + VOWEL_POST_FIX;
+        } else {
+            // if word starts with a consonant, add all consonants until the first vowel to the end and append postfix
+            final List<Character> moveToBack = new ArrayList<>();
+            final AtomicInteger vowelIndex = new AtomicInteger(-1);
+            for (int i = 0; i < charArr.length; i++) {
+                if (FIRST_CHAR_VOWELS.contains(charArr[i])) {
+                    vowelIndex.set(i);
+                    break;
+                } else {
+                    moveToBack.add(charArr[i]);
+                }
+            }
+
+            //if no vowel found, return as is
+            if (vowelIndex.get() < 0) {
+                return word;
+            }
+            final String ending = charListToString(moveToBack) + CONSONANT_POST_FIX;
+
+            final String constructedWord = word.substring(vowelIndex.get()) + ending;
+
+            return upperCase ?
+                    constructedWord.substring(0, 1).toUpperCase() + constructedWord.substring(1) : constructedWord;
+        }
+    }
+
+
+    protected boolean isAlphabetic(final char c) {
+        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+    }
+
+    protected String charListToString(final List<Character> charList) {
+        final StringBuilder builder = new StringBuilder(charList.size());
+        charList.forEach(builder::append);
+        return builder.toString();
     }
 }
--- src/test/java/com/plp/datapipeline/mappers/TestPigLatinMapper.java
+++ src/test/java/com/plp/datapipeline/mappers/TestPigLatinMapper.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-@SpringBootTest
 class TestPigLatinMapper {
 
     private PigLatinMapper mapper;
@@ -57,28 +56,28 @@ class TestPigLatinMapper {
     @Test
     void testInvalidWords() {
         final Map<String, Object > inputs = new HashMap<>();
-        inputs.put("a", "1Happy"); // rule: start with consonant + vowel
-        inputs.put("b", "C4hild"); // rule: start with 2 consonants
-        inputs.put("c", "Awesodsfame"); // rule: start with a vowel
+        inputs.put("a", "1Happy");
+        inputs.put("b", "C4hild");
+        inputs.put("c", "Awesod,sfame");
 
         final Map<String, Object> result = this.mapper.map(inputs);
 
         Assert.isTrue(result.size() == 3, "Same number of entries as input");
         Assert.isTrue(Objects.equals(result.get("a"), "1Happy"));
         Assert.isTrue(Objects.equals(result.get("b"), "C4hild"));
-        Assert.isTrue(Objects.equals(result.get("c"), "Awesodsfame"));
+        Assert.isTrue(Objects.equals(result.get("c"), "Awesod,sfame"));
     }
 
     @Test
     void testSentences() {
         final Map<String, Object > inputs = new HashMap<>();
-        inputs.put("a", "As a member of the Greek community, ...");
+        inputs.put("a", "As a member of the Greek community");
         inputs.put("b", "As1 a m3mb3r of zee Gr33k comminit!");
 
         final Map<String, Object> result = this.mapper.map(inputs);
 
-        Assert.isTrue(result.size() == 3, "Same number of entries as input");
-        Assert.isTrue(Objects.equals(result.get("a"), "asyay ayay embermay ofyay ethay eekgray ommunitycay"));
-        Assert.isTrue(Objects.equals(result.get("b"), "As1 ayay m3mb3r ofyay eezay Gr33k comminit!"));
+        Assert.isTrue(result.size() == 2, "Same number of entries as input");
+        Assert.isTrue(Objects.equals(result.get("a"), "Asway away embermay ofway ethay Eekgray ommunitycay"));
+        Assert.isTrue(Objects.equals(result.get("b"), "As1 away m3mb3r ofway eezay Gr33k comminit!"));
     }
 }

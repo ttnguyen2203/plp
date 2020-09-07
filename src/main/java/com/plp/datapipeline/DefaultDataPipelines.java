c4cbf59903aed10c383aa2527734a8b9083f5098
ttnguyen2203 <ttnguyen2203@berkeley.edu>
2020-09-06 17:28:45 -0700

    add default pipeline for piglatin translation

--- src/main/java/com/plp/datapipeline/DefaultDataPipelines.java
+++ src/main/java/com/plp/datapipeline/DefaultDataPipelines.java
@@ -1,6 +1,15 @@
 package com.plp.datapipeline;
 
+import com.plp.constants.DataPipelineConstants;
+import com.plp.datapipeline.consumers.IdentityConsumer;
+import com.plp.datapipeline.consumers.config.IdentityConsumerConfig;
+import com.plp.datapipeline.loaders.StringLoader;
+import com.plp.datapipeline.loaders.config.StringLoaderConfig;
+import com.plp.datapipeline.mappers.PigLatinMapper;
+import com.plp.datapipeline.mappers.config.PigLatinMapperConfig;
 import com.plp.dto.DataPipelineConfigDto;
+
+import java.util.Collections;
 
 public class DefaultDataPipelines {
 
@@ -8,7 +17,21 @@ public class DefaultDataPipelines {
 
     static {
 
+        // pig latin
         PIG_LATIN_PIPELINE = new DataPipelineConfigDto();
+        final StringLoaderConfig loaderConfig = new StringLoaderConfig();
+        loaderConfig.setLoaderClass(StringLoader.class);
+        loaderConfig.setLoadStringToKey("pigLatinString");
 
+        final PigLatinMapperConfig mapperConfig = new PigLatinMapperConfig();
+        mapperConfig.setMapperClass(PigLatinMapper.class);
+
+        final IdentityConsumerConfig consumerConfig = new IdentityConsumerConfig();
+        consumerConfig.setConsumerClass(IdentityConsumer.class);
+        PIG_LATIN_PIPELINE.setLoaderConfig(loaderConfig);
+        PIG_LATIN_PIPELINE.setMapperConfigs(Collections.singletonMap(
+                "pigLatinMapper", mapperConfig));
+        PIG_LATIN_PIPELINE.setConsumerConfigs(Collections.singletonMap(
+                DataPipelineConstants.PIG_LATIN_RESULT_CONSUMER_KEY, consumerConfig));
     }
 }
